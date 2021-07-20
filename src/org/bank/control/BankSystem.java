48bc3f09e89a8d37cfc25cdd2acc800e63d33e20
franklinmatheus <franklinmatheus1@gmail.com>
2021-07-20 01:38:24 -0300

    add: new class banksystem

---
+++ src/org/bank/control/BankSystem.java
@@ -0,0 +1,13 @@
+package org.bank.control;
+
+import org.bank.data.Database;
+
+public class BankSystem {
+	
+	private Database database;
+	
+	public BankSystem () {
+		this.database = new Database();
+	}
+
+}
